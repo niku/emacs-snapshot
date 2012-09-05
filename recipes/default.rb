3d10aa72c8590ffedf75a6f73485e3b699743160
niku <niku@niku.name>
2012-09-06 01:16:35 +0900

    implement for debian stable

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+apt_repository "emacs-snapshot" do
+  uri 'http://emacs.naquadah.org/'
+  distribution node['lsb']['codename']
+  components ['main']
+  key 'http://emacs.naquadah.org/key.gpg'
+end
+
+template "/etc/apt/sources.list.d/emacs-snapshot.list" do
+  notifies :run, resources(:execute => "apt-get update"), :immediately
+end
+
+package 'emacs-snapshot' do
+  action :install
+end
