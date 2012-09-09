438d3cd2b0921323b44141d68eb0083cc6533dd1
niku <niku@niku.name>
2012-09-09 21:39:49 +0900

    implement for ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,11 +6,22 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+case node[:platform]
+when 'debian'
 apt_repository "emacs-snapshot" do
   uri 'http://emacs.naquadah.org/'
   distribution node['lsb']['codename']
   components ['main']
   key 'http://emacs.naquadah.org/key.gpg'
+end
+when 'ubuntu'
+  apt_repository "emacs-snapshot" do
+    uri "http://ppa.launchpad.net/cassou/emacs/ubuntu"
+    distribution node['lsb']['codename']
+    components ["main"]
+    keyserver "keyserver.ubuntu.com"
+    key "CEC45805"
+  end
 end
 
 template "/etc/apt/sources.list.d/emacs-snapshot.list" do
